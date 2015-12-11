689fa05c872388fe14cf2d0a2901eb38bb6b4263
sparkofreason <dave.d.dixon@gmail.com>
2015-12-11 10:44:46 -0800

    Bootify, update deps

--- project.clj
+++ project.clj
@@ -1,42 +1,26 @@
 (defproject datascript-todo "0.1.0"
-  :dependencies [
-    [org.clojure/clojure "1.7.0"]
-    [org.clojure/clojurescript "1.7.122"]
-    [datascript "0.13.0"]
-    [datascript-transit "0.2.0"]
-    [rum "0.4.0"]
-  ]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.189"]
+                 [datascript "0.13.3"]
+                 [datascript-transit "0.2.0"]
+                 [rum "0.6.0"]
+                 [griebenschmalz "0.1.0"]]
 
-  :plugins [
-    [lein-cljsbuild "1.1.0"]
-  ]
+  :plugins [[lein-cljsbuild "1.1.0"]]
 
-  :cljsbuild { 
-    :builds [
-      { :id "advanced"
-        :source-paths  ["src"]
-        :compiler {
-          :main          datascript-todo.core
-          :output-to     "target/todo.js"
-          :optimizations :advanced
-          :pretty-print  false
-        }}
-  ]}
-  
-  :profiles {
-    :dev {
-      :cljsbuild {
-        :builds [
-          { :id "none"
-            :source-paths  ["src"]
-            :compiler {
-              :main          datascript-todo.core
-              :output-to     "target/todo.js"
-              :output-dir    "target/none"
-              :optimizations :none
-              :source-map    true
-            }}
-      ]}
-    }
-  }
-)
+  :cljsbuild {:builds [{:id           "advanced"
+                        :source-paths ["src"]
+                        :compiler     {:main          datascript-todo.core
+                                       :output-to     "target/todo.js"
+                                       :optimizations :advanced
+                                       :pretty-print  false}}]}
+
+  :profiles {:dev
+             {:cljsbuild
+              {:builds [{:id           "none"
+                         :source-paths ["src"]
+                         :compiler     {:main          datascript-todo.core
+                                        :output-to     "target/todo.js"
+                                        :output-dir    "target/none"
+                                        :optimizations :none
+                                        :source-map    true}}]}}})
